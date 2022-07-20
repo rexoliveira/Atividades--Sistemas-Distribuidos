595a83e5d83b1be60b02928ad61cafe5ddf9fa84
rexoliveira <roolivei@yahoo.com.br>
2022-07-19 21:50:47 -0300

    Configurações java no desktop lab.401

--- Atividade1-Prof.Luciano-Refeita-Atividade-Arquivos/src/atividade_realizada_2/ArquivoRunnable.java
+++ Atividade1-Prof.Luciano-Refeita-Atividade-Arquivos/src/atividade_realizada_2/ArquivoRunnable.java
@@ -6,34 +6,37 @@ import java.util.Scanner;
 
 public class ArquivoRunnable implements Runnable {
 
-    Scanner arquivo;
-    String nome ;
-    String nome_arquivo;
+  Scanner arquivo;
+  String nome;
+  String nome_arquivo;
 
+  public ArquivoRunnable(String nome, String nome_arquivo) {
+    this.nome = nome;
+    this.nome_arquivo = nome_arquivo;
+  }
 
-    public ArquivoRunnable(String nome, String nome_arquivo){
-        this.nome =  nome;
-        this.nome_arquivo = nome_arquivo;
-
-    }
-
-    @Override
-    public void run() {
-        try {
-            arquivo = new Scanner(new File(nome_arquivo));
-            int numero_linha = 1;
-            while (arquivo.hasNextLine()) {
-                String linha = arquivo.nextLine();
-                if (linha.contains(nome)) {
-                    System.out.println(nome_arquivo + "-" + linha + "-" + numero_linha);
-                }
-                numero_linha++;
-            }
-
-        } catch (FileNotFoundException ex) {
-            System.out.println("ERRO de arquivo");
+  @Override
+  public void run() {
+    try {
+      arquivo = new Scanner(new File(nome_arquivo));
+      int numero_linha = 1;
+      while (arquivo.hasNextLine()) {
+        String linha = arquivo.nextLine();
+        if (linha.contains(nome)) {
+          System.out.println(
+            Thread.currentThread().getName() +
+            "-" +
+            nome_arquivo +
+            "-" +
+            linha +
+            "-" +
+            numero_linha
+          );
         }
-
+        numero_linha++;
+      }
+    } catch (FileNotFoundException ex) {
+      System.out.println("ERRO de arquivo");
     }
-
+  }
 }
--- Atividade1-Prof.Luciano-Refeita-Atividade-Arquivos/src/atividade_realizada_2/InicioThred.java
+++ Atividade1-Prof.Luciano-Refeita-Atividade-Arquivos/src/atividade_realizada_2/InicioThred.java
@@ -6,17 +6,17 @@ public class InicioThred {
 
         /* Primeira Tarefa */
         ArquivoRunnable thread1 = new ArquivoRunnable("Dr", "nomes1.txt");
-        Thread t1 = new Thread(thread1);
+        Thread t1 = new Thread(thread1, "#TH1");
         t1.start();
 
         /* Segunda Tarefa */
         ArquivoRunnable thread2 = new ArquivoRunnable("Ana", "nomes2.txt");
-        Thread t2 = new Thread(thread2);
+        Thread t2 = new Thread(thread2,"#TH2");
         t2.start();
 
         /* Terceira Tarefa */
         ArquivoRunnable thread3 = new ArquivoRunnable("Sr", "nomes3.txt");
-        Thread t3 = new Thread(thread3);
+        Thread t3 = new Thread(thread3,"#TH3");
         t3.start();
 
     }
