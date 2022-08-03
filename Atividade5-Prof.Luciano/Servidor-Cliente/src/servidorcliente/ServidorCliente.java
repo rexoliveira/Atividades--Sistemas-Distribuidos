d0f1f4bab22e0a54cd1e6d0514a31b586b29d347
rexoliveira <roolivei@yahoo.com.br>
2022-08-03 17:58:47 -0300

    Adicionei Novo servidor com portas diferentes e Alertas

--- Atividade5-Prof.Luciano/Servidor-Cliente/src/servidorcliente/ServidorCliente.java
+++ Atividade5-Prof.Luciano/Servidor-Cliente/src/servidorcliente/ServidorCliente.java
@@ -1,11 +1,19 @@
 package servidorcliente;
 
+import java.awt.Color;
+import java.awt.Rectangle;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -13,12 +21,15 @@ import java.util.logging.Logger;
  */
 public class ServidorCliente extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Teste
-     */
     public ServidorCliente() {
         initComponents();
         new Thread(new Servidor()).start();
+    }
+
+//    Criar novo server após start do primeiro
+    public ServidorCliente(String porta) throws IOException {
+        initComponents();
+        new Thread(new Servidor(porta)).start();
     }
 
     public class Servico implements Runnable {
@@ -34,13 +45,13 @@ public class ServidorCliente extends javax.swing.JFrame {
         public void run() {
             try {
                 entrada = new Scanner(socket.getInputStream());
-               txaServer.append("Cliente " + socket.getPort() + " conectou."+"\n");
+                txaServer.append("Cliente " + socket.getPort() + " conectou." + "\n");
                 while (entrada.hasNextLine()) {
 //                System.out.println(socket);
 //                    System.out.println(entrada.nextLine());
-                    txaServer.append(entrada.nextLine()+"\n");
+                    txaServer.append(entrada.nextLine() + "\n");
                 }
-                txaServer.append("=============================================="+"\n");
+                txaServer.append("==============================================" + "\n");
             } catch (IOException ex) {
                 Logger.getLogger(Servico.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -52,23 +63,35 @@ public class ServidorCliente extends javax.swing.JFrame {
 
         private ServerSocket servidor;
         private boolean conectado = false;
+        private String porta = "";
 
         public Servidor() {
             try {
-                servidor = new ServerSocket(12345);
+                servidor = new ServerSocket(Integer.parseInt(txfPortServer.getText()));
                 conectado = true;
                 Mensagem("Servidor conectado.");
                 txaServer.append(("Servidor conectado.\n").toUpperCase());
-                
-                txaServer.append("#####################################################"+"\n");
-                                
-                txaServer.append(("Porta Servidor = " + servidor.getLocalPort()+".\n").toUpperCase());
-                
-                txaServer.append("#####################################################"+"\n");
+
+                txaServer.append("#####################################################" + "\n");
+
+                txaServer.append(("Porta Servidor = " + servidor.getLocalPort() + ".\n").toUpperCase());
+
+                txaServer.append("#####################################################" + "\n");
 
             } catch (IOException ex) {
 
             }
+        }
+
+        //Criar Novo servidor
+        public Servidor(String porta) throws IOException {
+            servidor = new ServerSocket(Integer.parseInt(porta));
+            conectado = true;
+            Mensagem("Servidor conectado.");
+            txaServer.append(("Servidor conectado.\n").toUpperCase());
+            txaServer.append("#####################################################" + "\n");
+            txaServer.append(("Porta Servidor = " + servidor.getLocalPort() + ".\n").toUpperCase());
+            txaServer.append("#####################################################" + "\n");
         }
 
         @Override
@@ -87,7 +110,7 @@ public class ServidorCliente extends javax.swing.JFrame {
         }
 
         private void Mensagem(String msg) {
-            
+
             System.out.println(msg);
         }
 
@@ -100,6 +123,9 @@ public class ServidorCliente extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         txaServer = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txfPortServer = new javax.swing.JTextField();
+        btnStartServer = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAlwaysOnTop(true);
@@ -121,6 +147,26 @@ public class ServidorCliente extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Saída Servidor");
 
+        jLabel2.setText("Porta:");
+
+        txfPortServer.setBackground(new java.awt.Color(0, 0, 0));
+        txfPortServer.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        txfPortServer.setForeground(new java.awt.Color(102, 255, 51));
+        txfPortServer.setText("12345");
+        txfPortServer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txfPortServerActionPerformed(evt);
+            }
+        });
+
+        btnStartServer.setText("Start Novo Servidor");
+        btnStartServer.setToolTipText("Antes de dar Start em um Novo Servidor troque a PORTA!");
+        btnStartServer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStartServerActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -128,24 +174,86 @@ public class ServidorCliente extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnStartServer)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txfPortServer, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txfPortServer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)
+                            .addComponent(btnStartServer))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 595, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txfPortServerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txfPortServerActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txfPortServerActionPerformed
+
+    private void btnStartServerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartServerActionPerformed
+        if (txfPortServer.getText().equals("12345")) {
+
+            JDialog dialog = new JOptionPane("Altere a porta para um valor diferente " + txfPortServer.getText() + ".",
+                    JOptionPane.INFORMATION_MESSAGE,
+                    JOptionPane.DEFAULT_OPTION).createDialog("Trocar Porta");
+            dialog.setAlwaysOnTop(true);
+            dialog.setVisible(true);
+
+            txfPortServer.requestFocus(true);
+            Border border;
+            
+            
+            int delay = 10000;   // tempo de espera antes da 1ª execução da tarefa.
+            int interval = 10000;  // intervalo no qual a tarefa será executada.
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                public void run() {
+                    
+                    txfPortServer.setBorder(new LineBorder(Color.GREEN));
+                }
+            }, delay, interval);
+            txfPortServer.setBorder(new LineBorder(Color.RED));
+        } else {
+
+            try {
+                ServidorCliente servidorCliente = new ServidorCliente(txfPortServer.getText());
+
+//        centro da tela
+                servidorCliente.setLocationRelativeTo(null);
+
+                servidorCliente.setVisible(true);
+//            Define a porta do atual servidor
+                servidorCliente.txfPortServer.setText(txfPortServer.getText());
+                servidorCliente.btnStartServer.setVisible(false);
+                servidorCliente.txfPortServer.setEnabled(false);
+            } catch (IOException ex) {
+                Logger.getLogger(ServidorCliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //Volta para PORTA padrão
+            txfPortServer.setText("12345");
+        }
+    }//GEN-LAST:event_btnStartServerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -184,8 +292,11 @@ public class ServidorCliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnStartServer;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea txaServer;
+    private javax.swing.JTextField txfPortServer;
     // End of variables declaration//GEN-END:variables
 }
